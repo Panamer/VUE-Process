ad1101c7525081cc1f7ce7d648027d556229aaf2
guoyongwei <guoyongwei@gomefinance.com.cn>
2020-05-06 10:54:43 +0800

    上传第二节课的代码

---
+++ 20200430/vue-study-web18/src/components/form/KInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 管理数据：实现双绑 -->
+    <!-- :value, @input -->
+    <input :type="type" :value="value" @input="onInput"
+      v-bind="$attrs">
+  </div>
+</template>
+
+<script>
+  export default {
+    inheritAttrs: false , // 关闭特性继承
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'text'
+      }
+    },
+    methods: {
+      onInput(e) {
+        this.$emit('input', e.target.value)
+
+        // 值发生变化的时候就是需要校验的时候
+        this.$parent.$emit('validate')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
