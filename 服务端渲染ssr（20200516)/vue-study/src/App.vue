11fb6936bfae9ba6beb94aad65e15893de174a07
guoyongwei <guoyongwei@gomeholdings.com.cn>
2020-05-25 11:04:37 +0800

    补充课件

---
+++ 服务端渲染ssr（20200516)/vue-study/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello, Vue!"></HelloWorld>
+    <p @click="$store.commit('add')">{{$store.state.count}}</p>
+    <nav>
+      <router-link to="/">index</router-link>
+      <router-link to="/detail">detail</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  asyncData({store, route}) {
+    // 调用者会传入store和route实例
+    return store.dispatch('getCount')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
